43eede6d7fe3dee670879eeb6554dc67a4023cd6
mpage68 <mikepage68@live.com>
2019-01-03 12:48:00 -0500

    odd

--- ExercisesApp/Program.cs
+++ ExercisesApp/Program.cs
@@ -11,7 +11,9 @@ namespace ExercisesApp
         static void Main(string[] args)
         {
             GetFibonacciNumberInRange(12);
-            GetEvenFibonacciNumber(12);
+            GetAllFibonacciNumbersInRange(12);
+            GetEvenFibonacciNumbersInRange(12);
+            GetOddFibonacciNumbersInRange(12);
             Console.ReadLine();
         }
 
@@ -21,23 +23,40 @@ namespace ExercisesApp
             int previousPreviousNumber = 0;
             int currentNumber = 1;
 
-            Console.WriteLine("Display number at position " + n + " in Fibonacci sequence...");
+            Console.WriteLine("Display number at position " + n + " in Fibonacci sequence...\n");
             for(int i = 1; i < n; i++)
             {
                 previousPreviousNumber = previousNumber;
                 previousNumber = currentNumber;
                 currentNumber = previousNumber + previousPreviousNumber;
             }
-                Console.WriteLine(currentNumber);            
+                Console.WriteLine(currentNumber + "\n");            
         }
 
-        public static int GetEvenFibonacciNumber(int n)
+        public static int GetAllFibonacciNumbersInRange(int n)
         {
             int ppn = 0;
             int pn = 0;
             int cn = 1;
 
-            Console.WriteLine("Display even numbers in range...");
+            Console.WriteLine("Display all numbers in sequence up to position " + n + " ...\n");
+            for(int i= 1; i < n; i++)
+            {
+                ppn = pn;
+                pn = cn;
+                cn = ppn + pn;  
+            Console.WriteLine(cn);
+            }
+            return cn;
+        }
+
+        public static int GetEvenFibonacciNumbersInRange(int n)
+        {
+            int ppn = 0;
+            int pn = 0;
+            int cn = 1;
+
+            Console.WriteLine("Display even numbers in range up to position " + n + " ...\n");
             for(int i = 1; i < n; i++)
             {
                 ppn = pn;
@@ -47,6 +66,27 @@ namespace ExercisesApp
                 if(cn % 2 == 0)
                 {
                 Console.WriteLine(cn);
+                }
+            }
+                return cn;
+        }
+
+        public static int GetOddFibonacciNumbersInRange(int n)
+        {
+            int ppn = 0;
+            int pn = 0;
+            int cn = 1;
+
+            Console.WriteLine("Display odd numbers in range up to position " + n + " ...\n");
+            for(int i = 1; i < n; i++)
+            {
+                ppn = pn;
+                pn = cn;
+                cn = ppn + pn;
+
+                if(cn % 2 == 1)
+                {
+                    Console.WriteLine(cn);
                 }
             }
                 return cn;
