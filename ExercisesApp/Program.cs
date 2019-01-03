f13c99cc484bdb365802b1f35458668be43776e5
mpage68 <mikepage68@live.com>
2019-01-03 12:17:58 -0500

    even

--- ExercisesApp/Program.cs
+++ ExercisesApp/Program.cs
@@ -10,23 +10,46 @@ namespace ExercisesApp
     {
         static void Main(string[] args)
         {
-            getFibonacciNumber(12);
+            GetFibonacciNumberInRange(12);
+            GetEvenFibonacciNumber(12);
             Console.ReadLine();
         }
 
-        public static void getFibonacciNumber(int n)
+        public static void GetFibonacciNumberInRange(int n)
         {
             int previousNumber = 0;
             int previousPreviousNumber = 0;
             int currentNumber = 1;
 
+            Console.WriteLine("Display number at position " + n + " in Fibonacci sequence...");
             for(int i = 1; i < n; i++)
             {
                 previousPreviousNumber = previousNumber;
                 previousNumber = currentNumber;
                 currentNumber = previousNumber + previousPreviousNumber;
-                Console.WriteLine(currentNumber);            
             }
+                Console.WriteLine(currentNumber);            
+        }
+
+        public static int GetEvenFibonacciNumber(int n)
+        {
+            int ppn = 0;
+            int pn = 0;
+            int cn = 1;
+
+            Console.WriteLine("Display even numbers in range...");
+            for(int i = 1; i < n; i++)
+            {
+                ppn = pn;
+                pn = cn;
+                cn = ppn + pn;
+
+                if(cn % 2 == 0)
+                {
+                Console.WriteLine(cn);
+                }
+            }
+                return cn;
         }
     }
 }
