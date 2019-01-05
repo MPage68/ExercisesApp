3e0f03c76398c42b91cb386721aec00b4260723e
mpage68 <mikepage68@live.com>
2019-01-05 17:30:23 -0500

    all even

--- ExercisesApp/Program.cs
+++ ExercisesApp/Program.cs
@@ -10,10 +10,12 @@ namespace ExercisesApp
     {
         static void Main(string[] args)
         {
-            GetFibonacciNumberInRange(12);
+            GetFibonacciNumberInRange(2000);
             GetAllFibonacciNumbersInRange(12);
             GetEvenFibonacciNumbersInRange(12);
             GetOddFibonacciNumbersInRange(12);
+            GetSumOfAllFibonacciNumbersInRange(12);
+            GetSumOfAllEvenFibonacciNumbersInRange(12);
             Console.ReadLine();
         }
 
@@ -90,6 +92,46 @@ namespace ExercisesApp
                 }
             }
                 return cn;
+        }
+
+        public static int GetSumOfAllFibonacciNumbersInRange(int n)
+        {
+            int ppn = 0;
+            int pn = 0;
+            int cn = 1;
+            int sum = 0;
+
+            Console.WriteLine("Display the sum of numbers in range up to position " + n + " ...\n");
+            for(int i = 1; i < n; i++)
+            {
+                ppn = pn;
+                pn = cn;
+                cn = ppn + pn;
+                sum += cn;
+            }
+            Console.WriteLine(sum);
+            return sum;
+        }   
+        
+        public static int GetSumOfAllEvenFibonacciNumbersInRange(int n)
+        {
+            int ppn = 0;
+            int pn = 0;
+            int cn = 1;
+            int sum = 0;
+
+            Console.WriteLine("Display sum of all even numbers in Fibonacci sequence up to " + n);
+            for(int i = 1; i < n; i++)
+            {
+                ppn = pn;
+                pn = cn;
+                cn = ppn + pn;
+
+                if (cn % 2 == 0)
+                    sum += cn;
+            }
+            Console.WriteLine(sum);
+            return sum;
         }
     }
 }
